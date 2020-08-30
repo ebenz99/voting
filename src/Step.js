4381aceb1a7d3a744a016bc94c6051d527236188
Ethan Bensman <ebenz99@gmail.com>
2020-08-29 20:55:03 -0400

    first commit

---
+++ src/Step.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Step.css';
+
+function Step() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="container-head">
+          <p class="container-head-text"> Hi </p>
+        </div>
+        <div class="container-body">
+          <p class="container-body-text"> Hello </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Step;
