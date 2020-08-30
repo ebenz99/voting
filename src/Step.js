10f264a48aa5c84617b725fb37bafa805cc29064
Ethan Bensman <ebenz99@gmail.com>
2020-08-29 22:14:12 -0400

    passing names via props

--- src/Step.js
+++ src/Step.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Step.css';
 
-function Step() {
-  return (
-    <div className="card-holder">
-      <div class="card">
-        <div class="card-head">
-          <p class="card-head-text"> Register </p>
-        </div>
-        <div class="card-body">
-          <p class="card-body-text"> Hello </p>
+class Step extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: true,
+    };
+    this.name = props.name;
+  }
+
+  render() {
+    return (
+      <div className="card-holder">
+        <div class="card">
+          <div class="card-head">
+            <p class="card-head-text"> {this.name} </p>
+          </div>
+          <div class="card-body">
+            <p class="card-body-text"> Hello </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Step;
--- src/index.js
+++ src/index.js
@@ -5,16 +5,27 @@ import Step from './Step';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <Step />
-    <Step />
-    <Step />
-    <Step />
+    <Step name='register'/>
+    <Step name='register'/>
+    <Step name='register'/>
+    <Step name='register'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// const stepNames = ['Register', 'Register', 'register', 'register'];
+// const items = []
+//
+// for (const [index, value] of elements.entries()) {
+//   items.push(<li key={index}>{value}</li>)
+// }
