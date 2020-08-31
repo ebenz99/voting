ff4168f0e8462b54442b9f9cb5e6639b18f4f104
Ethan Bensman <ebenz99@gmail.com>
2020-08-31 16:28:39 -0400

    Updates

---
+++ precache-manifest.1400cc9472a1a5c424658c5fcf9e55b2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4296c6159ced6a7a55d4a910f4599e32",
+    "url": "/voting/index.html"
+  },
+  {
+    "revision": "651b56c9c5d40d3a55c4",
+    "url": "/voting/static/css/main.723e41cc.chunk.css"
+  },
+  {
+    "revision": "2577df2ff9db3dcdb509",
+    "url": "/voting/static/js/2.505ee47b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voting/static/js/2.505ee47b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651b56c9c5d40d3a55c4",
+    "url": "/voting/static/js/main.27a0fc5a.chunk.js"
+  },
+  {
+    "revision": "a73667cf4c0f5a0128d3",
+    "url": "/voting/static/js/runtime-main.f0d70bed.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/voting/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/voting/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "5ea802ed893b931c99a0d7399dc8160f",
+    "url": "/voting/static/media/absenteeArrow.5ea802ed.svg"
+  },
+  {
+    "revision": "0eb588d3519c0355502436cc951219cc",
+    "url": "/voting/static/media/firstArrow.0eb588d3.svg"
+  },
+  {
+    "revision": "06915e58f6e2b76ff31788c2f1d3bc73",
+    "url": "/voting/static/media/inPersonArrow.06915e58.svg"
+  },
+  {
+    "revision": "fa9de17f602f41da191d2029f7e48791",
+    "url": "/voting/static/media/noArrow.fa9de17f.svg"
+  },
+  {
+    "revision": "f32c8eb664b754f953966762fa58424b",
+    "url": "/voting/static/media/registeredArrow.f32c8eb6.svg"
+  },
+  {
+    "revision": "1cdcae68120036f0950a4f7dba5f3dbb",
+    "url": "/voting/static/media/scflag.1cdcae68.svg"
+  },
+  {
+    "revision": "ad12e3a7eb0f49ff89366669969ce2de",
+    "url": "/voting/static/media/sentArrow.ad12e3a7.svg"
+  },
+  {
+    "revision": "2d5ea5738933bfc214db05bcbcde7d2f",
+    "url": "/voting/static/media/yesArrow.2d5ea573.svg"
+  }
+]);
